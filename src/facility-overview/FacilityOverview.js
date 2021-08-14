dc3ff18dd606ee55ae446c8ac3b3e872fa464bff
Billo Diallo <billodiallonet@gmail.com>
2021-08-14 13:46:28 +0300

    Add setFacility parameter

--- src/facility-overview/FacilityOverview.js
+++ src/facility-overview/FacilityOverview.js
@@ -3,3 +3,15 @@ import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+
+const FacilityOverview = ({ setFacility }) => {
+    const facilities = useSelector(state => state.facility.facilityCollection);
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default FacilityOverview;
