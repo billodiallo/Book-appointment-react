b414be1b9eac12622dbbc7bba9850ef9bdc0f79d
Billo Diallo <billodiallonet@gmail.com>
2021-08-17 11:20:38 +0300

    Import axios,fetchFacilitySuccess,Failure,logIn,fetchAppointment

---
+++ src/axios/requests.js
@@ -0,0 +1,7 @@
+import Axios from 'axios';
+import {
+  fetchFacilitySuccess,
+  fetchFacilityFailure,
+} from '../redux/actions/facilityAction';
+import logIn from '../redux/actions/userAction';
+import fetchAppointments from '../redux/actions/appointmentAction';
