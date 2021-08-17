514d1ac8aa39aa6ff8854646142e3a0bfeef0bd3
Billo Diallo <billodiallonet@gmail.com>
2021-08-17 11:22:22 +0300

    Add request object

--- src/axios/requests.js
+++ src/axios/requests.js
@@ -5,3 +5,11 @@ import {
 } from '../redux/actions/facilityAction';
 import logIn from '../redux/actions/userAction';
 import fetchAppointments from '../redux/actions/appointmentAction';
+
+const request = {
+  logUserIn: 'https://billo-appointment-app.herokuapp.com/api/v1/login',
+  SignUserIn: 'https://billo-appointment-app.herokuapp.com/api/v1/authentication',
+  facilityData: 'https://billo-appointment-app.herokuapp.com/api/v1/facilities',
+  setAppointment: 'https://billo-appointment-app.herokuapp.com/api/v1/appointments',
+  getAppointment: 'https://billo-appointment-app.herokuapp.com/api/v1/appointments',
+};
