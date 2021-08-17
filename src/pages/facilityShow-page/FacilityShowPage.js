65dd9de1a8fd46bf4f64735cb134cede7fc5c01d
Billo Diallo <billodiallonet@gmail.com>
2021-08-17 12:08:33 +0300

    Add FacilityShowPage css module,import  require file in FacilityShowPage.js

---
+++ src/pages/facilityShow-page/FacilityShowPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Redirect } from 'react-router';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import BookAppointmentForm from '../../components/book-appointment-form/BookAppointmentForm';
+import styles from './styles/FacilityShowPage.module.css';
+
