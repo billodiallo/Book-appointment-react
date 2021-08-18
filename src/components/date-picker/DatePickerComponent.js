a4ff80b84a52505d104055a81bbad41f92928782
Billo Diallo <billodiallonet@gmail.com>
2021-08-18 10:41:34 +0300

    Fix linter errors

--- src/components/date-picker/DatePickerComponent.js
+++ src/components/date-picker/DatePickerComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDatePicker from 'react-datepicker';
+import './styles/datepicker.css';
 
 const DatePickerComponent = ({ selectedDate, setSelectedDate }) => (
   <ReactDatePicker
