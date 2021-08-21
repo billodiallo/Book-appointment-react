12b723bcb9f258bf88dc6e2dabc55f54e1a69c57
Billo Diallo <billodiallonet@gmail.com>
2021-08-21 12:04:29 +0300

    Create appointment reducers,add appointmentReducer test

---
+++ src/tests/appointmentReducer.test.js
@@ -0,0 +1,5 @@
+import { FETCH_APPOINTMENTS } from '../redux/actions/actionType';
+import appointmentReducer from '../redux/reducers/appointmentReducer';
+
+describe('Appointment reducer', () => {
+  const initialState = [];
