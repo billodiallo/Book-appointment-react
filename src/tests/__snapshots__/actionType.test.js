a0e864fec54f51cc3d0e98debb4662da222ddeb0
Billo Diallo <billodiallonet@gmail.com>
2021-08-21 11:48:47 +0300

    Add LOg_OUT action test

--- src/tests/__snapshots__/actionType.test.js
+++
@@ -1,9 +0,0 @@
-import {
-    LOG_IN, LOG_OUT, FETCH_FACILITY_REQUEST, FETCH_FACILITY_SUCCESS,
-    FETCH_FACILITY_FAILURE, FETCH_APPOINTMENTS,
-  } from '../redux/actions/actionType';
-
-  describe('Action type', () => {
-    it('LOG_IN actionType should be a string', () => {
-      expect(LOG_IN).toEqual(expect.any(String));
-    });
